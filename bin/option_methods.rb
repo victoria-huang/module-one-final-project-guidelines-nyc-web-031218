31a446816f3e711fb182b53e3b9131603a1b54c1
victoria-huang <vctrah@gmail.com>
2018-03-27 15:44:24 -0400

    fixed option 2 if statement when there are no interactions

--- bin/option_methods.rb
+++ bin/option_methods.rb
@@ -21,8 +21,8 @@ def option_methods
     puts "Please enter the doctor's name:"
     doctor_name = gets.strip
 
-    patient.add_drug(full_drug_name, doctor_name)
-    menu
+    $patient.add_drug(full_drug_name, doctor_name)
+    option_methods
   when "2"
     patient.interactions = interactions_array
     #iterate through the interactions
@@ -44,4 +44,5 @@ def option_methods
    else
      puts "We found no interactions. Congrats!"
    end
+  end
  end
