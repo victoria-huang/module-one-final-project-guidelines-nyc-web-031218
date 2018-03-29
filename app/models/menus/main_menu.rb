478b996884d980222430a97acc9ddca89c8f88be
victoria-huang <vctrah@gmail.com>
2018-03-28 21:18:48 -0400

    changed wording in main menu

--- app/models/menus/main_menu.rb
+++ app/models/menus/main_menu.rb
@@ -3,11 +3,11 @@ require_relative '../../../config/environment'
 def main_menu
   puts "\nWhere would you like to go?\n\n"
   puts "Main Menu Options:"
-  puts "1. Prescriptions"
-  puts "2. Doctors"
-  puts "3. Reminders"
-  puts "4. Profile"
-  puts "5. Exit program\n\n"
+  puts "1. Prescriptions Menu"
+  puts "2. Doctors Menu"
+  puts "3. Reminders Menu"
+  puts "4. Profile Menu"
+  puts "5. Exit Program\n\n"
 
   response = gets.strip
 end
