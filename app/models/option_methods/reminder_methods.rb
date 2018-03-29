904a2ea05af24917231a9176a68cb84995fd940a
github username <jeremyagottfried@gmail.com>
2018-03-29 08:59:40 -0400

    fixed the reminder index issue

--- app/models/option_methods/reminder_methods.rb
+++ app/models/option_methods/reminder_methods.rb
@@ -60,7 +60,7 @@ def del_one_reminder_response(number)
   response = gets.strip
   case response
    when "yes", "y"
-     Reminder.delete_by(number.to_i - 1)
+     Reminder.delete(Reminder.all[number.to_i - 1].id)
      sleep(0.5)
      puts "Thank you, we have finished deleting your reminder\n"
      sleep(0.5)
