0d635effd585d8478fcf96ab9b867f23eeb1e902
victoria-huang <vctrah@gmail.com>
2018-03-29 09:13:33 -0400

    fixed check string empty

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ require 'bundler'
 Bundler.require
 
 def check_string(string)
-  string.scan(/\D/m).empty?
+  string.scan(/\D/).empty? && string != ""
 end
 
 ActiveRecord::Base.establish_connection(
